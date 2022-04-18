a41d7e0a073e19245d21330dd068dc9836868817
Mashadasha23 <97893139+Mashadasha23@users.noreply.github.com>
2022-04-18 17:58:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+// Create the reference variable of the canvas using fabric.Canvas()
+ my_canvas = new fabric.Canvas("myCanvas");
+
+// Define the starting width and height of the block images
+ block_width = 30;
+ block_hieght =30;
+
+
+// Define the starting x and y coordinates for the player
+ player_x = 10;
+ player_y = 10;
+
+
+// Define a variable named player_object to store object of the player image
+  var player_object = "";
+  var block_object ="";
+
+
+// Add a function named player_update() to add the player image
+function player_update(){
+    fabric.Image.fromURL("player.png",function(IMG){
+        player_object = IMG;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        my_canvas.add(player_object);
+    });
+    
+}
+player_update();
+
+
+// Add a function named block_update() to add the different images as per the specific keys pressed
+function block_update(block_img){
+    fabric.Image.fromURL(block_img,function(IMG) {
+        block_object = IMG;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top:player_y,
+                left:player_x
+        });
+
+     my_canvas.add(block_object); 
+    });
+}
+block_update("roof.jpg");
